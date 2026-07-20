55fa6517155d86bb931e3e36f8e3a5c75f2108c8
Purushottam Gaikwad <gaikwadpurushottam74gmail.com>
2026-07-20 22:22:03 +0530

    Update client and server

--- server/db.js
+++ server/db.js
@@ -1,17 +1,22 @@
 import mysql from 'mysql2';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const db = mysql.createConnection({
-    host:'127.0.0.1',
-    user: 'root',
-    password:'Purushottam@786',
-    database : 'campusshare'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: { rejectUnauthorized: false }
 });
 
-db.connect((err)=>{
-    if(err){
-        console.error('DB error : ',err);
-    }else{
-        //  console.log("MySQL Connected");
+db.connect((err) => {
+    if (err) {
+        console.error('DB error : ', err);
+    } else {
+        console.log("MySQL Connected");
     }
 });
 
