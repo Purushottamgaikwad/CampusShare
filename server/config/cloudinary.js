8d8bcc006038d3f2308310174e4c467d4aba43fc
Purushottam Gaikwad <gaikwadpurushottam74gmail.com>
2026-07-22 14:49:43 +0530

    cloadinary changes

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -20,7 +20,11 @@ const storage = new CloudinaryStorage({
         } else if (req.originalUrl.includes("/post")) {
             folder = "campusshare/posts";
         }
-
+        console.log("Cloudinary config check:", {
+            cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+            api_key: process.env.CLOUDINARY_API_KEY,
+            api_secret: process.env.CLOUDINARY_API_SECRET ? "exists" : "MISSING"
+        });
         return {
             folder: folder,
             allowed_formats: ["jpg", "jpeg", "png", "webp"]
